276b2802aff238a698bef7772e6ba1fb99c17a64
Pheon-Dev <devpheon@gmail.com>
2022-11-07 02:44:36 +0300

    CHG: Check Mark to Lightning Bolt

--- zap.zsh
+++ zap.zsh
@@ -9,7 +9,7 @@ function zapsource() {
 function zapplug() {
     PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
     if [ ! -d "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" ]; then
-      echo "Installing $PLUGIN_NAME ..." && git clone "https://github.com/$1.git" "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" > /dev/null 2>&1 && echo " $PLUGIN_NAME " || echo "Failed to install : $PLUGIN_NAME"
+      echo "Installing $PLUGIN_NAME ..." && git clone "https://github.com/$1.git" "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" > /dev/null 2>&1 && echo "  $PLUGIN_NAME " || echo "Failed to install : $PLUGIN_NAME"
     fi
     zapsource "$HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || \
     zapsource "$HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh"
@@ -19,7 +19,7 @@ function zapplug() {
 function zapcmp() {
     PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
     if [ ! -d "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" ]; then
-        git clone "https://github.com/$1.git" "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" > /dev/null 2>&1 || echo "Failed to install : $PLUGIN_NAME"
+        git clone "https://github.com/$1.git" "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" > /dev/null 2>&1 && echo "  $PLUGIN_NAME " || echo "Failed to install : $PLUGIN_NAME"
         fpath+=$(ls $HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*)
         [ -f $HOME/.local/share/zap/.zccompdump ] && $HOME/.local/share/zap/.zccompdump
     fi
