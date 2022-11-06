c022af51d6f62ac3faab3f74dab6f10baaf30af9
Christian Chiarulli <chris.machine@pm.me>
2022-11-06 16:25:42 -0500

    fix: completion function

--- zap.zsh
+++ zap.zsh
@@ -22,12 +22,12 @@ function zapcmp() {
         # For completions
 		completion_file_path=$(ls $HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*)
 		fpath+="$(dirname "${completion_file_path}")"
-        zapsource "$HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh"
+        zapsource $HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh
     else
         git clone "https://github.com/$1.git" "$HOME/.local/share/zap/plugins/$PLUGIN_NAME"
 		fpath+=$(ls $HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*)
-        [ -f "$HOME/.local/share/zap/.zccompdump ] && $HOME/.local/share/zap/.zccompdump"
+        [ -f $HOME/.local/share/zap/.zccompdump ] && $HOME/.local/share/zap/.zccompdump
     fi
 	completion_file="$(basename "${completion_file_path}")"
-	if [ "$2" = true ] && compinit "${completion_file:1}"
+	[ "$2" = true ] && compinit "${completion_file:1}"
 }
