d39e9ab6e152629ec5f8d08da979012b3ade8b2c
David Isaksson <davidisaksson93@gmail.com>
2022-11-06 22:31:31 +0100

    chore: fix whitespace

--- install.sh
+++ install.sh
@@ -6,6 +6,5 @@ mkdir "$HOME/.local/share/zap/plugins"
 
 # shellcheck disable=SC2016
 if ! grep -q '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' "$HOME/.zshrc"; then
-  echo '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' >> "$HOME/.zshrc"
+    echo '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' >> "$HOME/.zshrc"
 fi
-
--- zap.zsh
+++ zap.zsh
@@ -7,7 +7,7 @@ function zapsource() {
 
 function zapplug() {
     PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
-    if [ -d "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" ]; then 
+    if [ -d "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" ]; then
         # For plugins
         zapsource "$HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || \
         zapsource "$HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh"
@@ -18,16 +18,16 @@ function zapplug() {
 
 function zapcmp() {
     PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
-    if [ -d "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" ]; then 
+    if [ -d "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" ]; then
         # For completions
-		completion_file_path=$(ls $HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*)
-		fpath+="$(dirname "${completion_file_path}")"
+        completion_file_path=$(ls $HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*)
+        fpath+="$(dirname "${completion_file_path}")"
         zapsource $HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh
     else
         git clone "https://github.com/$1.git" "$HOME/.local/share/zap/plugins/$PLUGIN_NAME"
-		fpath+=$(ls $HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*)
+        fpath+=$(ls $HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*)
         [ -f $HOME/.local/share/zap/.zccompdump ] && $HOME/.local/share/zap/.zccompdump
     fi
-	completion_file="$(basename "${completion_file_path}")"
-	[ "$2" = true ] && compinit "${completion_file:1}"
+    completion_file="$(basename "${completion_file_path}")"
+    [ "$2" = true ] && compinit "${completion_file:1}"
 }
