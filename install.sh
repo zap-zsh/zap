182744b66e416c1f44228ae88250c7a07b0b1a5f
David Isaksson <davidisaksson93@gmail.com>
2022-11-06 22:19:17 +0100

    fix: quote path

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ git clone https://github.com/zap-zsh/zap.git "$HOME/.local/share/zap"
 mkdir "$HOME/.local/share/zap/plugins"
 
 # shellcheck disable=SC2016
-if ! grep -q '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' $HOME/.zshrc; then
+if ! grep -q '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' "$HOME/.zshrc"; then
   echo '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' >> "$HOME/.zshrc"
 fi
 
