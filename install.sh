57aff9ecb5491c6db4ba4700c08aab24f1c2f88c
David Isaksson <davidisaksson93@gmail.com>
2022-11-06 23:00:32 +0100

    fix: minor whitespace and minor fixes #1

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ git clone https://github.com/zap-zsh/zap.git "$HOME/.local/share/zap"
 mkdir "$HOME/.local/share/zap/plugins"
 
 # shellcheck disable=SC2016
-if ! grep -q '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' $HOME/.zshrc; then
-  echo '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' >> "$HOME/.zshrc"
+if ! grep -q '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' "$HOME/.zshrc"; then
+    echo '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' >> "$HOME/.zshrc"
 fi
-
--- zap.zsh
+++ zap.zsh
@@ -5,29 +5,27 @@ function zapsource() {
     [ -f "$1" ] && source "$1"
 }
 
+# For plugins
 function zapplug() {
     PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
-    if [ -d "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" ]; then 
-        # For plugins
-        zapsource "$HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || \
-        zapsource "$HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh"
-    else
+    if [ ! -d "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" ]; then
         git clone "https://github.com/$1.git" "$HOME/.local/share/zap/plugins/$PLUGIN_NAME"
     fi
+    zapsource "$HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || \
+    zapsource "$HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh"
 }
 
+# For completions
 function zapcmp() {
     PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
-    if [ -d "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" ]; then 
-        # For completions
-		completion_file_path=$(ls $HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*)
-		fpath+="$(dirname "${completion_file_path}")"
-        zapsource $HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh
-    else
+    if [ ! -d "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" ]; then
         git clone "https://github.com/$1.git" "$HOME/.local/share/zap/plugins/$PLUGIN_NAME"
-		fpath+=$(ls $HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*)
+        fpath+=$(ls $HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*)
         [ -f $HOME/.local/share/zap/.zccompdump ] && $HOME/.local/share/zap/.zccompdump
     fi
-	completion_file="$(basename "${completion_file_path}")"
-	[ "$2" = true ] && compinit "${completion_file:1}"
+    completion_file_path=$(ls $HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*)
+    fpath+="$(dirname "${completion_file_path}")"
+    zapsource $HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh
+    completion_file="$(basename "${completion_file_path}")"
+    [ "$2" = true ] && compinit "${completion_file:1}"
 }
