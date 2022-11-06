ca254f9ed8e30cbd0b92c6a958b1331cdc240272
Christian Chiarulli <chris.machine@pm.me>
2022-11-06 15:26:15 -0500

    chore: add .gitignore

--- zap.zsh
+++ zap.zsh
@@ -20,12 +20,12 @@ function zapcmp() {
     PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
     if [ -d "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" ]; then 
         # For completions
-		completion_file_path=$(ls "$HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*")
+		completion_file_path=$(ls $HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*)
 		fpath+="$(dirname "${completion_file_path}")"
         zapsource "$HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh"
     else
         git clone "https://github.com/$1.git" "$HOME/.local/share/zap/plugins/$PLUGIN_NAME"
-		fpath+=$(ls "$HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*")
+		fpath+=$(ls $HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*)
         [ -f "$HOME/.local/share/zap/.zccompdump ] && $HOME/.local/share/zap/.zccompdump"
     fi
 	completion_file="$(basename "${completion_file_path}")"
