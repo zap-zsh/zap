03e8d618582f07aeed4fa724d46f43d011d75c3c
Christian Chiarulli <chris.machine@pm.me>
2022-11-06 14:16:38 -0500

    feat: rename some functions

--- zap.zsh
+++ zap.zsh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 # Function to source files if they exist
-function zsh_add_file() {
+function zapsource() {
     [ -f "$HOME/.config/zsh/$1" ] && source "$HOME/.config/zsh/$1"
 }
 
-function zapadd() {
+function zapplug() {
     PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
     if [ -d "$HOME/.config/zsh/plugins/$PLUGIN_NAME" ]; then 
         # For plugins
