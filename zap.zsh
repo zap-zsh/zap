1f7eef456a7d3b052d5e97bd0526e38055d407be
David Isaksson <davidisaksson93@gmail.com>
2022-11-07 09:13:01 +0100

    feat: add ZAP_PLUGIN_DIR environment variable
    This lowers the risk of typos as well as exposing the plugin directory
    to the user via the environment variable.
    
    The `ZAP_` prefix could be used for other variables as well in the
    future.

--- zap.zsh
+++ zap.zsh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export ZAP_PLUGIN_DIR="$HOME/.local/share/zap/plugins"
+
 # Function to source files if they exist
 function zapsource() {
     [ -f "$1" ] && source "$1"
@@ -8,27 +10,27 @@ function zapsource() {
 # For plugins
 function zapplug() {
     PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
-    if [ ! -d "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" ]; then
+    if [ ! -d "$ZAP_PLUGIN_DIR/$PLUGIN_NAME" ]; then
       echo "Installing $PLUGIN_NAME ..." && git clone "https://github.com/$1.git" \
-        "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" > /dev/null 2>&1 && echo " $PLUGIN_NAME " || echo "Failed to install : $PLUGIN_NAME"
+        "$ZAP_PLUGIN_DIR/$PLUGIN_NAME" > /dev/null 2>&1 && echo " $PLUGIN_NAME " || echo "Failed to install : $PLUGIN_NAME"
     fi
-    zapsource "$HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || \
-    zapsource "$HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh" || \
-    zapsource "$HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.zsh-theme"
+    zapsource "$ZAP_PLUGIN_DIR/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || \
+    zapsource "$ZAP_PLUGIN_DIR/$PLUGIN_NAME/$PLUGIN_NAME.zsh" || \
+    zapsource "$ZAP_PLUGIN_DIR/$PLUGIN_NAME/$PLUGIN_NAME.zsh-theme"
 }
 
 # For completions
 function zapcmp() {
     PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
-    if [ ! -d "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" ]; then
-        git clone "https://github.com/$1.git" "$HOME/.local/share/zap/plugins/$PLUGIN_NAME" \
+    if [ ! -d "$ZAP_PLUGIN_DIR/$PLUGIN_NAME" ]; then
+        git clone "https://github.com/$1.git" "$ZAP_PLUGIN_DIR/$PLUGIN_NAME" \
           > /dev/null 2>&1 && echo " $PLUGIN_NAME " || echo "Failed to install : $PLUGIN_NAME"
-        fpath+=$(ls $HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*)
+        fpath+=$(ls $ZAP_PLUGIN_DIR/$PLUGIN_NAME/_*)
         [ -f $HOME/.local/share/zap/.zccompdump ] && $HOME/.local/share/zap/.zccompdump
     fi
-    completion_file_path=$(ls $HOME/.local/share/zap/plugins/$PLUGIN_NAME/_*)
+    completion_file_path=$(ls $ZAP_PLUGIN_DIR/$PLUGIN_NAME/_*)
     fpath+="$(dirname "${completion_file_path}")"
-    zapsource $HOME/.local/share/zap/plugins/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh
+    zapsource $ZAP_PLUGIN_DIR/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh
     completion_file="$(basename "${completion_file_path}")"
     [ "$2" = true ] && compinit "${completion_file:1}"
 }
