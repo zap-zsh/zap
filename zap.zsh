804c83e1cb8f64852c0d9cf388081d9d633c8380
David Isaksson <davidisaksson93@gmail.com>
2022-11-07 09:19:55 +0100

    chore: substitute ~/.local/share/zap with ZAP_DIR, make variables local
    We don't want the internal variables to be available outside of the
    function scope, so let's mark them as local.
    
    Note: We might want to change the shebang to point to bash or zsh
    instead of plain sh since some of the things in this script isn't POSIX
    compliant.

--- zap.zsh
+++ zap.zsh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-export ZAP_PLUGIN_DIR="$HOME/.local/share/zap/plugins"
+export ZAP_DIR="$HOME/.local/share/zap"
+export ZAP_PLUGIN_DIR="$ZAP_DIR/plugins"
 
 # Function to source files if they exist
 function zapsource() {
@@ -9,28 +10,31 @@ function zapsource() {
 
 # For plugins
 function zapplug() {
-    PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
-    if [ ! -d "$ZAP_PLUGIN_DIR/$PLUGIN_NAME" ]; then
-      echo "Installing $PLUGIN_NAME ..." && git clone "https://github.com/$1.git" \
-        "$ZAP_PLUGIN_DIR/$PLUGIN_NAME" > /dev/null 2>&1 && echo " $PLUGIN_NAME " || echo "Failed to install : $PLUGIN_NAME"
+    local full_plugin_name="$1"
+    local plugin_name=$(echo "$full_plugin_name" | cut -d "/" -f 2)
+    if [ ! -d "$ZAP_PLUGIN_DIR/$plugin_name" ]; then
+      echo "Installing $plugin_name ..." && git clone "https://github.com/${full_plugin_name}.git" \
+        "$ZAP_PLUGIN_DIR/$plugin_name" > /dev/null 2>&1 && echo " $plugin_name " || echo "Failed to install : $plugin_name"
     fi
-    zapsource "$ZAP_PLUGIN_DIR/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh" || \
-    zapsource "$ZAP_PLUGIN_DIR/$PLUGIN_NAME/$PLUGIN_NAME.zsh" || \
-    zapsource "$ZAP_PLUGIN_DIR/$PLUGIN_NAME/$PLUGIN_NAME.zsh-theme"
+    zapsource "$ZAP_PLUGIN_DIR/$plugin_name/$plugin_name.plugin.zsh" || \
+    zapsource "$ZAP_PLUGIN_DIR/$plugin_name/$plugin_name.zsh" || \
+    zapsource "$ZAP_PLUGIN_DIR/$plugin_name/$plugin_name.zsh-theme"
 }
 
 # For completions
 function zapcmp() {
-    PLUGIN_NAME=$(echo $1 | cut -d "/" -f 2)
-    if [ ! -d "$ZAP_PLUGIN_DIR/$PLUGIN_NAME" ]; then
-        git clone "https://github.com/$1.git" "$ZAP_PLUGIN_DIR/$PLUGIN_NAME" \
-          > /dev/null 2>&1 && echo " $PLUGIN_NAME " || echo "Failed to install : $PLUGIN_NAME"
-        fpath+=$(ls $ZAP_PLUGIN_DIR/$PLUGIN_NAME/_*)
-        [ -f $HOME/.local/share/zap/.zccompdump ] && $HOME/.local/share/zap/.zccompdump
+    local full_plugin_name="$1"
+    local initialize_completion="$2"
+    local plugin_name=$(echo "$full_plugin_name" | cut -d "/" -f 2)
+    if [ ! -d "$ZAP_PLUGIN_DIR/$plugin_name" ]; then
+        git clone "https://github.com/${full_plugin_name}.git" "$ZAP_PLUGIN_DIR/$plugin_name" \
+          > /dev/null 2>&1 && echo " $plugin_name " || echo "Failed to install : $plugin_name"
+        fpath+=$(ls $ZAP_PLUGIN_DIR/$plugin_name/_*)
+        [ -f $ZAP_DIR/.zccompdump ] && $ZAP_DIR/.zccompdump
     fi
-    completion_file_path=$(ls $ZAP_PLUGIN_DIR/$PLUGIN_NAME/_*)
+    local completion_file_path=$(ls $ZAP_PLUGIN_DIR/$plugin_name/_*)
     fpath+="$(dirname "${completion_file_path}")"
-    zapsource $ZAP_PLUGIN_DIR/$PLUGIN_NAME/$PLUGIN_NAME.plugin.zsh
-    completion_file="$(basename "${completion_file_path}")"
-    [ "$2" = true ] && compinit "${completion_file:1}"
+    zapsource $ZAP_PLUGIN_DIR/$plugin_name/$plugin_name.plugin.zsh
+    local completion_file="$(basename "${completion_file_path}")"
+    [ "$initialize_completion" = true ] && compinit "${completion_file:1}"
 }
