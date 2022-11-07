4ea8c01494445f37b162ce9cb7d909eaf92399ba
Christian Chiarulli <chris.machine@pm.me>
2022-11-07 09:38:39 -0500

    fix: rollback progress, not workin on mac

--- install.sh
+++ install.sh
@@ -1,12 +1,7 @@
 #!/bin/sh
 
-source "./progress.zsh"
 
 function run_scripts {
-
-    CURRENT_PERCENTAGE=$1
-
-    DUMMY_CUMPUTING_DELAY=$(( ( RANDOM % 20 ) / 10 ))  # Replace by zero
 
     git clone https://github.com/zap-zsh/zap.git "$HOME/.local/share/zap" > /dev/null 2>&1
     mkdir -p "$HOME/.local/share/zap/plugins"
@@ -22,14 +17,7 @@ function run_scripts {
     if ! grep -q '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' "$zshrc"; then
         echo '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' >> "$zshrc"
     fi
-
-    sleep $DUMMY_CUMPUTING_DELAY
-
-    sleep $PROGRESS_DELAY
-
 }
 
-echo
-progress 0; progress 100
 echo
 echo " Zapped"
