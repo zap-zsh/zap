29edeee86637c67cae5d6e5e683b8e6e25bdcb69
David Isaksson <davidisaksson93@gmail.com>
2022-11-07 16:31:03 +0100

    fix: make install.sh POSIX compliant

--- install.sh
+++ install.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-
-function run_scripts {
-
+run_scripts() {
     git clone https://github.com/zap-zsh/zap.git "$HOME/.local/share/zap" > /dev/null 2>&1
     mkdir -p "$HOME/.local/share/zap/plugins"
 
     # check if ZDOTDIR is set, and if it is, check if ZDOTDIR/.zshrc exists
-    if [[ ! -z $ZDOTDIR ]] && [[ -f $ZDOTDIR/.zshrc ]]; then
+    if [ -n "$ZDOTDIR" ] && [ -f "$ZDOTDIR/.zshrc" ]; then
         zshrc="$ZDOTDIR/.zshrc"
     else
         zshrc="$HOME/.zshrc"
