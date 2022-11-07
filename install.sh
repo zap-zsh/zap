c5aed530b05fb1ad18f4073fe38dff50add1367b
dotzenith <sharmadanshu01@gmail.com>
2022-11-06 19:58:42 -0500

    added an additional check for zdotdir

--- install.sh
+++ install.sh
@@ -4,7 +4,14 @@ git clone https://github.com/zap-zsh/zap.git "$HOME/.local/share/zap"
 
 mkdir "$HOME/.local/share/zap/plugins"
 
+# check if ZDOTDIR is set, and if it is, check if ZDOTDIR/.zshrc exists
+if [[ ! -z $ZDOTDIR ]] && [[ -f $ZDOTDIR/.zshrc ]]; then
+    zshrc="$ZDOTDIR/.zshrc"
+else
+    zshrc="$HOME/.zshrc"
+fi
+
 # shellcheck disable=SC2016
-if ! grep -q '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' "$HOME/.zshrc"; then
-    echo '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' >> "$HOME/.zshrc"
+if ! grep -q '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' "$zshrc"; then
+    echo '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' >> "$zshrc"
 fi
