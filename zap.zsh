c6d105f62feacfd5ea74d42e04b3f3be7f902470
David Isaksson <davidisaksson93@gmail.com>
2022-11-09 21:21:05 +0100

    chore: add plugin_dir variable to clean

--- zap.zsh
+++ zap.zsh
@@ -14,18 +14,19 @@ function zapplug() {
     local full_plugin_name="$1"
     local initialize_completion="$2"
     local plugin_name=$(echo "$full_plugin_name" | cut -d "/" -f 2)
-    if [ ! -d "$ZAP_PLUGIN_DIR/$plugin_name" ]; then
+    local plugin_dir="$ZAP_PLUGIN_DIR/$plugin_name"
+    if [ ! -d "$plugin_dir" ]; then
       echo "Installing $plugin_name ..." && git clone "https://github.com/${full_plugin_name}.git" \
-        "$ZAP_PLUGIN_DIR/$plugin_name" > /dev/null 2>&1 && echo " $plugin_name " || echo "Failed to install : $plugin_name"
+        "$plugin_dir" > /dev/null 2>&1 && echo " $plugin_name " || echo "Failed to install : $plugin_name"
     fi
-    _try_source "$ZAP_PLUGIN_DIR/$plugin_name/$plugin_name.plugin.zsh"
-    _try_source "$ZAP_PLUGIN_DIR/$plugin_name/$plugin_name.zsh"
-    _try_source "$ZAP_PLUGIN_DIR/$plugin_name/$plugin_name.zsh-theme"
+    _try_source "$plugin_dir/$plugin_name.plugin.zsh"
+    _try_source "$plugin_dir/$plugin_name.zsh"
+    _try_source "$plugin_dir/$plugin_name.zsh-theme"
     local completion_file_path=$(ls $ZAP_PLUGIN_DIR/$plugin_name/_*)
     if [ -f "$completion_file_path" ]; then
-        fpath+=$(ls $ZAP_PLUGIN_DIR/$plugin_name/_*)
+        fpath+=$(ls $plugin_dir/_*)
         [ -f $ZAP_DIR/.zccompdump ] && $ZAP_DIR/.zccompdump
-        fpath+="$(dirname $ZAP_PLUGIN_DIR/$plugin_name)"
+        fpath+="$(dirname $plugin_dir)"
         local completion_file="$(basename "$completion_file_path")"
         [ "$initialize_completion" = true ] && compinit "${completion_file:1}"
     fi
