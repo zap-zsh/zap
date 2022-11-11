cfacd4358057254437cf0e7d9de80b2a0b18543b
Christian Chiarulli <chris.machine@pm.me>
2022-11-11 17:25:34 -0500

    fix: don't namespace plugins under username

--- zap.zsh
+++ zap.zsh
@@ -3,33 +3,39 @@
 export ZAP_DIR="$HOME/.local/share/zap"
 export ZAP_PLUGIN_DIR="$ZAP_DIR/plugins"
 
-function plug() {
+_try_source() {
+    # shellcheck disable=SC1090
+    [ -f "$1" ] && source "$1"
+}
+
+plug() {
 	plugin="$1"
     if [ -f "$plugin" ]; then
 		source "$plugin"
 	else
-		local full_path="${ZAP_PLUGIN_DIR}/${plugin}"
-		local plugin_dir="$(dirname $full_path)"
-		local plugin_name="$(basename $full_path)"
-		if [ ! -d "$full_path" ]; then
-			echo "🔌$plugin_name"
-			git clone "https://github.com/${plugin}.git" "$full_path" > /dev/null 2>&1
-			if [[ ! -z $2 ]]; then                                              # check if the second arg of plug exist
-				cd $plugin_dir && git checkout "$2" > /dev/null 2>&1 && cd      # checkout the desidered commit
-			fi
-			if [ $? -ne 0 ]; then
-				echo "Failed to install $plugin_name"
-				exit 1
-			fi
-			echo -e "\e[1A\e[K⚡$plugin_name"
-		fi
-		source "${full_path}/${plugin_name}.zsh" > /dev/null 2>&1 || \
-		source "${full_path}/${plugin_name}.plugin.zsh" > /dev/null 2>&1 || \
-		source "${full_path}/${plugin_name}.zsh-theme" > /dev/null 2>&1 
-	fi
+    local full_plugin_name="$1"
+    local initialize_completion="$2"
+    local plugin_name=$(echo "$full_plugin_name" | cut -d "/" -f 2)
+    local plugin_dir="$ZAP_PLUGIN_DIR/$plugin_name"
+    if [ ! -d "$plugin_dir" ]; then
+        echo "🔌$plugin_name"
+        git clone "https://github.com/${full_plugin_name}.git" "$plugin_dir" > /dev/null 2>&1
+        if [[ ! -z $2 ]]; then                                              # check if the second arg of plug exist
+            cd $plugin_dir && git checkout "$2" > /dev/null 2>&1 && cd      # checkout the desidered commit
+        fi
+        if [ $? -ne 0 ]; then
+            echo "Failed to install $plugin_name"
+            exit 1
+        fi
+        echo -e "\e[1A\e[K⚡$plugin_name"
+    fi
+    _try_source "$plugin_dir/$plugin_name.plugin.zsh"
+    _try_source "$plugin_dir/$plugin_name.zsh"
+    _try_source "$plugin_dir/$plugin_name.zsh-theme"
+  fi
 }
 
-function _pull () {
+_pull () {
     echo "🔌$1"
     git pull > /dev/null 1>&1
     if [ $? -ne 0 ]; then
@@ -107,7 +113,7 @@ Version () {
   echo "⚡Zap Version v$ver" 
 }
 
-function zap() {
+zap() {
     local command="$1"
     if [[ "$command" == "-v" ]] || [[ "$command" == "--version" ]]; then
        Version;
