5504f8adb67030adcc7010b3cd9b274b48d2b804
Christian Chiarulli <chris.machine@pm.me>
2022-11-10 20:14:33 -0500

    chore: remove completion from zapplug as it isn't needed

--- zap.zsh
+++ zap.zsh
@@ -25,14 +25,6 @@ function zapplug() {
     _try_source "$plugin_dir/$plugin_name.plugin.zsh"
     _try_source "$plugin_dir/$plugin_name.zsh"
     _try_source "$plugin_dir/$plugin_name.zsh-theme"
-    local completion_file_path=$(find "$plugin_dir" -maxdepth 1 -type f -name "_*" | head -1)
-    if [ -f "$completion_file_path" ]; then
-        fpath+=$("$completion_file_path")
-        [ -f "$ZAP_DIR/.zccompdump" ] && "$ZAP_DIR/.zccompdump"
-        fpath+="$(dirname "$plugin_dir")"
-        local completion_file="$(basename "$completion_file_path")"
-        [ "$initialize_completion" = true ] && compinit "${completion_file:1}"
-    fi
 }
 
 update () {
