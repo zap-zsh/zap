30293e0513036e1dc379f99a710afba829cfa3a8
mamaraddio <kotvmrc@gmail.com>
2022-12-07 16:51:39 +0100

    test

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 main() {
-    git clone https://github.com/zap-zsh/zap.git "$HOME/.local/share/zap" > /dev/null 2>&1
+    git clone https://github.com/${1:-'zap-zsh'}/zap.git "$HOME/.local/share/zap" > /dev/null 2>&1
     mkdir -p "$HOME/.local/share/zap/plugins"
 
     # check if ZDOTDIR is set, and if it is, check if ZDOTDIR/.zshrc exists
