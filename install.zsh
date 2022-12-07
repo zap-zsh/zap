9b3ecbec892428a551b206fbcd11a7de0337e661
mamaraddio <kotvmrc@gmail.com>
2022-12-07 17:02:37 +0100

    test

---
+++ install.zsh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+
+main() {
+    local USR=$1
+    local DEFAULT='zap-zsh'
+    git clone "https://github.com/${USR:-$DEFAULT}/zap.git" "$HOME/.local/share/zap" > /dev/null 2>&1
+    mkdir -p "$HOME/.local/share/zap/plugins"
+
+    ###tis is a test
+
+    # check if ZDOTDIR is set, and if it is, check if ZDOTDIR/.zshrc exists
+    if [ -n "$ZDOTDIR" ] && [ -f "$ZDOTDIR/.zshrc" ]; then
+        zshrc="$ZDOTDIR/.zshrc"
+    else
+        zshrc="$HOME/.zshrc"
+    fi
+    touch "$zshrc"
+
+    # shellcheck disable=SC2016
+    if ! grep -q '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' "$zshrc"; then
+        sed -i '1 i[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' "$zshrc"
+    fi
+}
+
+main
+echo " Zapped"
+
+# vim: ft=bash ts=4 et
