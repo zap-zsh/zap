4706e1da22f76b95923d565c4ca7918dbbb6084a
mamaraddio <kotvmrc@gmail.com>
2023-01-09 17:23:52 +0100

    Executed shfmt

--- install.zsh
+++ install.zsh
@@ -2,19 +2,19 @@
 # shellcheck disable=SC2016
 
 main() {
-	local ZAP_DIR="$HOME/.local/share/zap"
-	local ZSHRC="${ZDOTDIR:-$HOME}/.zshrc"
-	[[ $1 == "--branch" || $1 == "-b" ]] && local BRANCH="$2"
+    local ZAP_DIR="$HOME/.local/share/zap"
+    local ZSHRC="${ZDOTDIR:-$HOME}/.zshrc"
+    [[ $1 == "--branch" || $1 == "-b" ]] && local BRANCH="$2"
 
     git clone -b "${BRANCH:-"master"}" https://github.com/zap-zsh/zap.git "$ZAP_DIR" > /dev/null 2>&1 || return 12 # Unable to clone
-	mkdir -p "$ZAP_DIR/plugins"
+    mkdir -p "$ZAP_DIR/plugins"
 
-	if ! grep -q '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' "$ZSHRC"; then
-		sed -i.old '1 i\
+    if ! grep -q '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' "$ZSHRC"; then
+        sed -i.old '1 i\
 [ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' "$ZSHRC"
-	fi
-	echo " Zapped"
-	return 0
+    fi
+    echo " Zapped"
+    return 0
 }
 
 main $@
