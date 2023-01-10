4f597c7702446082a51dd3c4232b9c13a463cf57
Marco <82162277+mamaraddio@users.noreply.github.com>
2023-01-10 02:00:55 +0100

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -1,12 +1,11 @@
 #!/usr/bin/env zsh
-# shellcheck disable=SC2016
 
 main() {
     local ZAP_DIR="$HOME/.local/share/zap"
     local ZSHRC="${ZDOTDIR:-$HOME}/.zshrc"
     [[ $1 == "--branch" || $1 == "-b" ]] && local BRANCH="$2"
 
-    git clone -b "${BRANCH:-"master"}" https://github.com/zap-zsh/zap.git "$ZAP_DIR" > /dev/null 2>&1 || return 12 # Unable to clone
+    git clone -b "${BRANCH:-"master"}" https://github.com/zap-zsh/zap.git "$ZAP_DIR" > /dev/null 2>&1 || return
     mkdir -p "$ZAP_DIR/plugins"
 
     if ! grep -q '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' "$ZSHRC"; then
@@ -20,7 +19,3 @@ main() {
 main $@
 
 # vim: ft=zsh ts=4 et
-# Return codes:
-#   0:  Success
-#   12: Failed to clone
-#   13: Failed to checkout
