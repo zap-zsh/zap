c70c4896ac58ca0389f0e9191125b8812e2969c8
mamaraddio <kotvmrc@gmail.com>
2023-01-12 11:48:53 +0100

    feat: improved `zap version`

--- zap.zsh
+++ zap.zsh
@@ -83,11 +83,9 @@ COMMANDS:
 }
 
 function _zap_version() {
-    # local -r BLUE="\033[1;34m" GREEN="\033[1;32m" RESET="\033[0m"
     local -Ar color=(BLUE "\033[1;34m" GREEN "\033[1;32m" RESET "\033[0m")
     local _ver=${$(git -C $ZAP_DIR describe --tags HEAD)%%-*} _branch=$(git -C "$ZAP_DIR" branch --show-current) _commit=${$(git -C $ZAP_DIR describe --tags HEAD)##*-}
     echo "⚡ Zap v${color[GREEN]}${_ver}${color[RESET]}\nBranch:\t${color[GREEN]}${_branch}${color[RESET]}\nCommit:\t${color[BLUE]}${_commit#g}${color[RESET]}"
-    # echo "⚡ Zap Version is ${GREEN}v${_ver}${RESET} on commit ${BLUE}${_commit#g}${RESET}"
 }
 
 function zap() {
