4fc2dbfd3dceb63ee0b54bd7a9a96d1d88c67722
Matthew Winter <33818+wintermi@users.noreply.github.com>
2023-01-17 09:42:17 +1100

    feat: cleanup directories (#104)
    * fix: remove the duplicate and misspelt directory '.github/ISSUE_TAMPLATE'
    
    * feat: move the plugins directory to be part of the repo

--- install.zsh
+++ install.zsh
@@ -17,7 +17,6 @@ main() {
     }
 
     git clone -b "${BRANCH:-master}" https://github.com/zap-zsh/zap.git "$ZAP_DIR" > /dev/null 2>&1 || { echo "❌ Failed to install Zap" && return 2 }
-    mkdir -p "$ZAP_DIR/plugins"
 
 # @formatter:off
 	if ! grep -q '[ -f "$HOME/.local/share/zap/zap.zsh" ] && source "$HOME/.local/share/zap/zap.zsh"' "$ZSHRC"; then
